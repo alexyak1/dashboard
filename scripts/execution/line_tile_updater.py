11ebce53d455c5c484fa2dfd7f47ff1dcc1624d9
Aleksandr Iakimchuk <aleksandr.iakimchuk@ericsson.com>
2019-07-18 19:32:59 +0200

    delete unworked code

--- scripts/execution/line_tile_updater.py
+++ scripts/execution/line_tile_updater.py
@@ -1,5 +1,4 @@
-import requests as req
-import tile 
+import tile
 import json
 
 def update_tile(line_chart_id, percentage_chart_id, params, include_ongoing = False):
@@ -16,22 +15,7 @@ def update_tile(line_chart_id, percentage_chart_id, params, include_ongoing = Fa
     data_json = json.dumps(line_chart_content)
     tile.update_tile('line_chart', line_chart_id, data_json)
 
-    percentage_chart_content = {
-        'title': extractedData["rstate"],
-        'subtitle': 'Finshed',
-        'big_value': str(success_rate) + '%',
-        'left_value': str(extractedData["passed"]),
-        'right_value': str(extractedData["total"] - extractedData["excluded"]),
-        'left_label': 'Passed',
-        'right_label': 'Total'
-    } 
-    data_json = json.dumps(percentage_chart_content)
-    tile.update_tile('simple_percentage', percentage_chart_id, data_json)
-
     if int(success_rate) > 98:
         tile.default_line_config(line_chart_id)
     elif int(success_rate) <= 98:
         tile.line_config_alert(line_chart_id)
-
-    color_config_json = tile.percentage_chart_color_config(success_rate)
-    tile.update_tile_config(percentage_chart_id, color_config_json)
