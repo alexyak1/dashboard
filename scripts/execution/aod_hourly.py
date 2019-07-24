bacc8ddc28b7fa7209697d25a3dc15572a28bbb4
Aleksandr Iakimchuk <aleksandr.iakimchuk@ericsson.com>
2019-07-24 14:04:39 +0200

    split tiles

--- scripts/execution/aod_hourly.py
+++ scripts/execution/aod_hourly.py
@@ -1,7 +1,7 @@
 import line_tile_updater as updater
 
 def main():
-    line_chart_id = 'advanced_chart_aod'
+    line_chart_id = 'aod_hourly'
     params = 'ciOfficial=true&program=AOD&createdTimestamp=-3&testLoopType=HOURLY&aggregated=&column=testLoop&column2=rstate&row=hardware'
     updater.update_tile(line_chart_id, params)
 
